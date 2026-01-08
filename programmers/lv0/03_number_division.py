d6ddab613eb57137dd18f7ee17c72b90f469d6fe
beco <galashow7@gmail.com>
2026-01-08 17:47:32 +0900

    Lv. 0 3번 수 나누기

---
+++ programmers/lv0/03_number_division.py
@@ -0,0 +1,26 @@
+# 문제 설명
+# 2자리 이상의 정수 number가 주어집니다. 주어진 코드는 이 수를 2자리씩 자른 뒤, 자른 수를 모두 더해서 그 합을 출력하는 코드입니다. 코드가 올바르게 작동하도록 한 줄을 수정해 주세요.
+
+# 제한사항
+# 10 ≤ number ≤ 2,000,000,000
+# number의 자릿수는 2의 배수입니다.
+
+# number = int(input())
+#
+# answer = 0
+#
+# for i in range(1):
+#     answer += number % 100
+#     number //= 100
+#
+# print(answer)
+
+number = int(input())
+
+answer = 0
+
+for i in range(len(str(number)) // 2):
+    answer += number % 100
+    number //= 100
+
+print(answer)

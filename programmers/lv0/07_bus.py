f3e48ed0b14a6a6ac6e1c5b65304cfaef7b4e5af
beco <galashow7@helixtech.co.kr>
2026-01-29 10:32:27 +0900

    시인성 더 확보

--- programmers/lv0/07_bus.py
+++ programmers/lv0/07_bus.py
@@ -33,10 +33,10 @@ def func4(station):
 def solution(seat, passengers):
     num_passenger = 0
     for station in passengers:
-        num_passenger += func3(station)
+        num_passenger += func4(station)
 
-        num_passenger -= func4(station)
+        num_passenger -= func3(station)
 
-    answer = func1(seat + num_passenger)
+    answer = func1(seat - num_passenger)
 
     return answer
